98eaf18e6989fc52513e3351e3db467ac2cb3817
Jay (Joon-young) Gwak <46766754+jgwak1@users.noreply.github.com>
2020-12-25 00:42:28 -0500

    Update preprocess_and_models.py

--- preprocess_and_models.py
+++ preprocess_and_models.py
@@ -25,18 +25,21 @@ def stem_by_row(Input):
     x = ' '.join([stemmer.stem(word) for word in Input.split()])
     return x 
 
-def transcript_preprocessor(data):
+def transcript_preprocessor(data, trasncript_colname = 'transcript'):
+
     from nltk.corpus import stopwords    
     import string 
     punctuations = re.escape(string.punctuation)
 
     stop = stopwords.words('english')
-    stop.append('im')
-    
-    data['transcript'] = data.apply(lambda row: row['transcript'].lower(), axis=1)
-    data['transcript'] = data.apply(lambda row: re.sub(r'['+punctuations+']', '', row['transcript']), axis= 1)
-    data['transcript'] = data.apply(lambda row:  stop_word_remover(row['transcript'],stop), axis= 1)
-    data['transcript'] = data.apply(lambda row: stem_by_row(row['transcript']), axis= 1)
+    stop.append('\n')
+    stop.append('n')
+
+
+    data[trasncript_colname] = data.apply(lambda row: row[trasncript_colname].lower(), axis=1)
+    data[trasncript_colname] = data.apply(lambda row: re.sub(r'['+punctuations+']', '', row[trasncript_colname]), axis= 1)
+    data[trasncript_colname] = data.apply(lambda row:  stop_word_remover(row[trasncript_colname],stop), axis= 1)
+    data[trasncript_colname] = data.apply(lambda row: stem_by_row(row[trasncript_colname]), axis= 1)
 
     return data
 
@@ -46,15 +49,14 @@ def transcript_preprocessor(data):
 # LDA /w
 
 
-def topic_modeling_by_candidate(data, candidate_list):
+def topic_modeling_by_candidate(data, candidate_list, trasncript_colname = 'TRANSCRIPT' ):
 
-    data = data[ (data.TRANSCRIPT!="longer public video") & (data.TRANSCRIPT!="noneyoutub") ]
-    
+    data = data[ (data[trasncript_colname]!="longer public video") & (data[trasncript_colname]!="noneyoutub") ]
     
     result_df = pd.DataFrame()
     
     #cv = CountVectorizer(max_df=0.5, min_df=3, stop_words='english') # max_df and min_f are hyperparatmeters dealwith this part 
-    
+
     #cv = CountVectorizer(max_df=0.5, min_df=3 )     # "max_df==1.0(default)" means ignore terms that appear in more than 100% of the documents (default setting doesn't ignore any terms)
                                                      # 'min_df==1 (default)' means ignore terms that appear in less than 1 document. (default setting does not ignore any terms)
                                                      #   When these parameters are changed, it can lead to cases where too much pruning occurs.
@@ -64,9 +66,9 @@ def topic_modeling_by_candidate(data, candidate_list):
     
     for candidate in candidate_list:
         
-        cand_data = data[data.CANDIDATES==candidate]
+        cand_data = data[data['CANDIDATES']==candidate]
     
-        cv_fit = cv.fit_transform( cand_data["TRANSCRIPT"] )  #can extracture feature from cv after this
+        cv_fit = cv.fit_transform( cand_data[trasncript_colname] )  #can extracture feature from cv after this
         feature = cv.get_feature_names() # words 
         
         lda = LatentDirichletAllocation(n_components=10, random_state= 1)
@@ -88,3 +90,53 @@ def topic_modeling_by_candidate(data, candidate_list):
         
         
     return result_df  
+
+
+
+
+def TopXWords_fromEntire(data, X, vocab_start_col_idx = False):
+    
+    if (vocab_start_col_idx == False):
+        raise ValueError("Error: Carefully check the column index where which the vocabularies start, and specify it.")
+
+
+    M = list(zip(list(data.iloc[:,vocab_start_col_idx:].columns), list(data.iloc[:,vocab_start_col_idx:].sum())))
+    dt = pd.DataFrame(sorted(M, key = lambda x: x[1], reverse=True))
+
+    return dt[0:X]
+
+
+
+
+
+
+
+def TopXTopics_TopYDocuments_Generator(data, X, Y, trasncript_colname = "TRANSCRIPT" ):
+    
+    # First apply LDA
+    cv = TfidfVectorizer(max_df=0.5, min_df= 3 ) 
+    cv_fit_transformed = cv.fit_transform(data[trasncript_colname])
+    feature = cv.get_feature_names()
+    lda = LatentDirichletAllocation(n_components = X, random_state =1 )
+    ldamod = lda.fit(cv_fit_transformed)
+    score_document_topic = pd.DataFrame(ldamod.transform(cv_fit_transformed)) # Row: document, Column: topic
+    score_topic_words = pd.DataFrame(ldamod.components_)  # Row: topic, Column: words(feature)
+    score_topic_words.columns = feature 
+    
+    result_df = pd.DataFrame([])
+    
+    
+    # For Top X Topics, get Top Y Documents , and make it into dataframe
+    for i in range(X):
+    
+        x= pd.DataFrame(score_document_topic[i]).sort_values(by=i, ascending=False)[0:Y]
+        x = pd.DataFrame(data.loc[data.index.intersection(x.index),:][trasncript_colname])
+        x.columns = ["topic_"+str(i)]
+        x = x.reset_index().drop(columns=["index"])
+        
+        result_df = pd.concat([result_df, x], axis = 1) 
+        
+    return result_df
+    
+    
+
